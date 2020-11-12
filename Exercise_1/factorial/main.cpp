550561b6cbe3843e5abeb39074792507ab8b506c
Maximilian Gaul <yz64edok@stud.informatik.uni-erlangen.de>
2020-11-12 09:11:55 +0100

    Finished factorial exercise

--- Exercise_1/factorial/main.cpp
+++ Exercise_1/factorial/main.cpp
@@ -1,3 +1,41 @@
+#include "math.h"
+#include <cmath>
+#include <iostream>
+
+double factorial(const int nmbr) {
+
+	// if(nmbr == 0 || nmbr == 1) {
+	// 	return 1.0;
+	// }
+
+	// const double euler = std::exp(1.0);
+
+	// return sqrt(2.0 * M_PI * nmbr) * pow(nmbr / euler,  nmbr);
+
+	double result = 1.0;
+	for(int i = 1; i <= nmbr; ++i) {
+		result = result * i;
+	}
+
+	return result;
+}
+
 int main(int argc, char **args) {
-	return 0;
+
+	int fac_nmbr;
+
+	std::cout << "Hello, this is factorial!" << std::endl;
+	std::cout << "Please enter your number: ";
+
+	std::cin >> fac_nmbr;
+
+	if( std::cin.good() ) {
+
+		const double fac = factorial(fac_nmbr);
+
+		std::cout << "Estimated factorial: " << fac << std::endl;
+
+	} else {
+		std::cout << "Unfortunately, we encountered an unexpected input." << std::endl;
+	}
 }
